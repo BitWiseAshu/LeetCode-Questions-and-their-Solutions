ce1bcdbfa853c964f3aec96fe4ed60610b4facfd
Om Nath Dixit <115968079+Ashu-Pablo@users.noreply.github.com>
2023-10-02 12:59:18 +0530

    Added solution - LeetHub

--- Number of distinct subsequences - GFG/number-of-distinct-subsequences.java
+++ Number of distinct subsequences - GFG/number-of-distinct-subsequences.java
@@ -24,9 +24,9 @@ class GFG {
 class Solution {
     int distinctSubsequences(String s) {
         // code here
-        /*
+        
         int n = s.length();
-        int mod = 100000007;
+        int mod = 1000000007;
         
         int dp[] = new int[n+1];
         dp[0] = 1;
@@ -50,36 +50,7 @@ class Solution {
         int ans = (dp[n] + mod) % mod;
         
         return ans;
-        */
         
-        
-        
-        int mod = 1000000007;
-        int n = s.length();
-        
-        // dp[i] represents the number of distinct subsequences of s[0:i]
-        int[] dp = new int[n + 1];
-        dp[0] = 1; // Empty string is a subsequence of any string
-        
-        // Map to store the last seen index of each character in s
-        Map<Character, Integer> lastSeen = new HashMap<>();
-        
-        for (int i = 1; i <= n; i++) {
-            dp[i] = (2 * dp[i - 1]) % mod; // Doubling the previous count
-            
-            char currentChar = s.charAt(i - 1);
-            
-            // If the current character has been seen before, subtract the count of subsequences
-            if (lastSeen.containsKey(currentChar)) {
-                int prevIndex = lastSeen.get(currentChar);
-                dp[i] = (dp[i] - dp[prevIndex - 1] + mod) % mod;
-            }
-            
-            // Update the last seen index of the current character
-            lastSeen.put(currentChar, i);
-        }
-        
-        // Subtract 1 to exclude the empty string
-        return (dp[n] + mod) % mod;
+    
     }
 }
