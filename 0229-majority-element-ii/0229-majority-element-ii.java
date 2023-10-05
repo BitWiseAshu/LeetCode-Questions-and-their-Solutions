a4c806ad026cd986e084a377b129197f432652b2
Om Nath Dixit <115968079+Ashu-Pablo@users.noreply.github.com>
2023-10-05 23:51:34 +0530

    Time: 13 ms (21.73%), Space: 46.1 MB (89.97%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -7,7 +7,7 @@ class Solution {
         int k = n/3;
         
         for(int i = 0; i < n; i++){
-                map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
         
         for(Map.Entry<Integer,Integer> entry : map.entrySet()){
