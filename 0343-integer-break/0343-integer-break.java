2d0491fb69ba3b2baa4549986ebf2369ab7c7378
Om Nath Dixit <115968079+Ashu-Pablo@users.noreply.github.com>
2023-10-06 23:59:39 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (17.26%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+
+        
+        int remainder = n % 3;
+        int quotient = n / 3;
+        if (remainder == 1) {
+            quotient--;
+            remainder = 4;
+        }
+        
+        int result = (int) Math.pow(3, quotient);
+
+        if (remainder != 0) {
+            result *= remainder;
+        }
+        
+        return result;
+    }
+}
