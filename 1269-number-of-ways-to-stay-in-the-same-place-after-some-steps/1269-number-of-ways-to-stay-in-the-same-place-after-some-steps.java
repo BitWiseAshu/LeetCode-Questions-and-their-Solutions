28088091cbca8c76f5b0614df8518f685c826b34
Om Nath Dixit <115968079+Ashu-Pablo@users.noreply.github.com>
2023-10-15 13:19:53 +0530

    Time: 9 ms (75.61%), Space: 43.1 MB (68.90%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.java
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.java
@@ -21,4 +21,43 @@ class Solution {
         }       
         return dp[0][steps];
     }
+    
+    
+    // This is also the right solution but it gives time limit Exceed
+    /*
+    public int numWays(int steps, int arrLen) {
+        int MOD = 1000000007;
+        int maxPosition = Math.min(steps / 2, arrLen - 1);
+        
+        // Create a memoization array to store previously computed results
+        int[][] memo = new int[steps + 1][maxPosition + 1];
+        
+        // Call the recursive helper function
+        return helper(steps, 0, maxPosition, memo, MOD);
+    }
+    
+    private int helper(int steps, int position, int maxPosition, int[][] memo, int MOD) {
+        // Base cases
+        if (steps == 0) {
+            return (position == 0) ? 1 : 0;
+        }
+        if (position < 0 || position > maxPosition) {
+            return 0;
+        }
+        
+        // Check if the result is already computed
+        if (memo[steps][position] != 0) {
+            return memo[steps][position];
+        }
+        
+        // Calculate the number of ways recursively
+        int ways = helper(steps - 1, position, maxPosition, memo, MOD); // Stay in the same place
+        ways = (ways + helper(steps - 1, position - 1, maxPosition, memo, MOD)) % MOD; // Move one step to the left
+        ways = (ways + helper(steps - 1, position + 1, maxPosition, memo, MOD)) % MOD; // Move one step to the right
+        
+        // Store the result in the memoization array and return it
+        memo[steps][position] = ways;
+        return ways;
+    }
+    */
 }
