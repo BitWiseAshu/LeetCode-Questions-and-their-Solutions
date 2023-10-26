91bd1b74bfff7d10bd6718d3a49115c625a5c326
Om Nath Dixit <115968079+Ashu-Pablo@users.noreply.github.com>
2023-10-26 11:05:37 +0530

    Added solution - LeetHub

--- Minimum Operations - GFG/minimum-operations.java
+++ Minimum Operations - GFG/minimum-operations.java
@@ -28,19 +28,32 @@ class Solution
     public int minOperation(int n)
     {
         //code here.
-        int count  = 0;
+        // int count  = 0;
         
-        while (n > 0){
-            if(n%2 == 0){
-                n = n/2;
-            }
-            else{
-                n = n-1;
-            }
+        // while (n > 0){
+        //     if(n%2 == 0){
+        //         n = n/2;
+        //     }
+        //     else{
+        //         n = n-1;
+        //     }
             
-            count  ++;
+        //     count  ++;
+        // }
+        // return count;
+        
+        if(n <=0 ){
+            return 0;
         }
-        return count;
+        
+        if(n%2 == 0){
+            return 1+minOperation(n/2);
+        }
+        else{
+            return 1 + minOperation(n-1);
+        }
+        
+        
     }
     
 }
