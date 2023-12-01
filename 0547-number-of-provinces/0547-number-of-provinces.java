77b5d552ae7f634dac7dc76552eef9b9557de97e
Om Nath Dixit <115968079+Ashu-Pablo@users.noreply.github.com>
2023-12-01 12:30:42 +0530

    Time: 1 ms (94.89%), Space: 46.7 MB (8.72%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,4 +1,8 @@
 class Solution {
+    
+    
+    //  SOLUTION USING BFS
+    /*
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         boolean visited[] = new boolean[n];
@@ -29,5 +33,34 @@ class Solution {
         }
         
         return count;
+    }
+    
+    */
+    
+    
+    // SOLUTION USING DFS
+    
+    public void dfs(int node, int[][] isConnected, boolean[] visit) {
+        visit[node] = true;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (isConnected[node][i] == 1 && !visit[i]) {
+                dfs(i, isConnected, visit);
+            }
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int numberOfComponents = 0;
+        boolean[] visit = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                numberOfComponents++;
+                dfs(i, isConnected, visit);
+            }
+        }
+
+        return numberOfComponents;
     }
 }
