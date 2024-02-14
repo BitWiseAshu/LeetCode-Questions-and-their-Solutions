8f9c8d8b340c86c6048fd3d72660c6f465853639
Om Nath Dixit <115968079+Ashu-Pablo@users.noreply.github.com>
2024-02-14 12:50:45 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (89.72%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,6 +13,10 @@
  *     }
  * }
  */
+
+
+// Using Recursion preorder
+/*
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
@@ -27,5 +31,44 @@ class Solution {
         }
 
         return ans;
+    }
+}
+*/
+
+
+
+
+// Using Morris Traversal
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> inOrder = new ArrayList<>();
+        TreeNode current = root;
+        
+        while(current != null){
+            if(current.left == null) {
+                inOrder.add(current.val);
+                current = current.right;
+            }
+            else{
+                TreeNode prev = current.left;
+                
+                while(prev.right != null && prev.right != current){
+                    prev = prev.right;
+                }
+                
+                if(prev.right == null){
+                    prev.right = current;
+                    current = current.left;
+                }
+                else{
+                    prev.right = null;
+                    inOrder.add(current.val);
+                    current  = current.right;
+                }
+            }
+        }
+        
+        return inOrder;
     }
 }
