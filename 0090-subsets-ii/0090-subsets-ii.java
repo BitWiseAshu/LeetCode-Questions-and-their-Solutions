f082ddce7ed5f8e18b6144cb79cd0f2d0db909f0
Om Nath Dixit <115968079+Ashu-Pablo@users.noreply.github.com>
2024-02-20 16:39:59 +0530

    Time: 9 ms (9.05%), Space: 43.3 MB (80.48%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -24,3 +24,42 @@ class Solution {
         subsetsWithDupHelper(ans, nums, list, n+1);
     }
 }
+
+
+
+// public class Solution {
+
+//     public void findSubsets(int left, int right, int size, int[] nums, List<List<Integer>> arr) {
+//         List<Integer> temp = new ArrayList<>();
+
+//         for (int i = left; i <= right; i++) {
+//             temp.add(nums[i]);
+//         }
+
+//         if (!arr.contains(temp)) {
+//             arr.add(new ArrayList<>(temp));
+//         }
+
+//         if (left == right) {
+//             return;
+//         }
+
+//         int mid = -1;
+//         if (size % 2 == 1) {
+//             mid = (size + 1) / 2;
+//         } else {
+//             mid = size / 2;
+//         }
+
+//         findSubsets(0, mid - 1, mid, nums, arr);
+//         findSubsets(mid, nums.length - 1, mid, nums, arr);
+//     }
+         
+//     public List<List<Integer>> subsetsWithDup(int[] nums) {
+//         List<List<Integer>> arr = new ArrayList<>();
+//         Arrays.sort(nums);
+//         findSubsets(0, nums.length - 1, nums.length, nums, arr);
+//         return arr;
+//     }
+
+// }
