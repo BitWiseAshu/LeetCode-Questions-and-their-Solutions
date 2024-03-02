db141cc82e26c9c660d584754588bb15aca63442
Om Nath Dixit <115968079+BitWiseAshu@users.noreply.github.com>
2024-03-03 00:07:14 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (64.80%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,4 +1,6 @@
 class Solution {
+    // 1st solution
+    /*
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1){
@@ -15,4 +17,42 @@ class Solution {
         
         return dp[n-1];
     }
+    */
+    
+    
+    // 2nd solution
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1){
+            return nums[0];
+        }
+        
+        int dp[] = new int[n];
+        Arrays.fill(dp, -1);
+        
+        int ans = robHelper(n-1, nums, dp);
+        return ans;
+    }
+    
+    public int robHelper(int n, int []nums, int []dp){
+        if(n < 0){
+            return 0;
+        }
+        
+        if(n == 0){
+            return nums[n];
+        }
+        
+        if (dp[n] != -1) return dp[n];
+        
+        // Calculate the maximum sum by either picking the current element or not picking it.
+        int pick = nums[n] + robHelper(n - 2, nums, dp);
+        // System.out.println("picked "+ pick + " n " + n);
+        int nonPick = robHelper(n - 1, nums, dp);
+        // System.out.println("non-picked"+ nonPick+" n " + n);
+        
+        dp[n] = Math.max(pick, nonPick);
+        return dp[n];
+    }
+    
 }
