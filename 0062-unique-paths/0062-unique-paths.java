72e9a4677d3e9aca68e87dce96e6480265fb0a6a
Om Nath Dixit <115968079+BitWiseAshu@users.noreply.github.com>
2024-03-10 02:44:52 +0530

    Time: 1 ms (11.06%), Space: 40.3 MB (50.32%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,5 @@
 // Memoization approach
+/*
 class Solution {
     
     public int uniquePathsUsingMemoization(int i, int j, int [][]dp){
@@ -28,5 +29,78 @@ class Solution {
         }
         
         return uniquePathsUsingMemoization(m-1, n-1, dp);
+    }
+}
+*/
+
+
+// Recursion Approach
+/*
+class Solution {
+    
+    public int uniquePathsUsingMemoization(int i, int j){
+        if(i == 0 && j == 0){
+            return 1;
+        }
+        
+        if(i < 0 || j < 0){
+            return 0;
+        }
+        
+        int up = uniquePathsUsingMemoization(i-1, j);
+        int down = uniquePathsUsingMemoization(i, j-1);
+        
+        return up+down;
+    }
+    public int uniquePaths(int m, int n) {
+        
+        return uniquePathsUsingMemoization(m-1, n-1);
+    }
+}
+*/
+
+
+
+
+// Tabulation approach
+
+class Solution {
+    
+    public int uniquePaths(int m, int n) {
+        int [][]dp = new int[m][n];
+        
+        for(int []arr : dp){
+            Arrays.fill(arr, -1);
+        }
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                int up = 0;
+                int left = 0;
+                
+                if(i > 0 && i < m){
+                    up = dp[i-1][j];
+                }
+                if(j > 0 && j < n){
+                    left = dp[i][j-1];
+                }
+                
+                dp[i][j] = up + left;
+            }
+        }
+        
+        // for(int i = 0; i < m; i++){
+        //     for(int j = 0; j < n; j++){
+        //         System.out.print(dp[i][j]+ " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        return dp[m-1][n-1];
     }
 }
