b7e49b277e0557778f6daa2dcd274ee5c3e57c80
Om Nath Dixit <115968079+BitWiseAshu@users.noreply.github.com>
2024-03-10 02:47:32 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (50.32%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -63,7 +63,7 @@ class Solution {
 
 
 // Tabulation approach
-
+/*
 class Solution {
     
     public int uniquePaths(int m, int n) {
@@ -102,5 +102,49 @@ class Solution {
         // }
         
         return dp[m-1][n-1];
+    }
+}
+*/
+
+
+
+
+// Space optimization 
+
+class Solution {
+    
+    public int uniquePaths(int m, int n) {
+        int []prev = new int[n];
+        
+        for (int i = 0; i < m; i++) {
+            // Create a temporary array to store the results for the current row
+            int temp[] = new int[n];
+            
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    // Base condition: There's one way to reach the top-left cell (0, 0)
+                    temp[j] = 1;
+                    continue;
+                }
+
+                int up = 0;
+                int left = 0;
+
+                // Calculate the number of ways by moving up (if possible) and left (if possible)
+                if (i > 0)
+                    up = prev[j];
+                if (j > 0)
+                    left = temp[j - 1];
+
+                // Store the total number of ways to reach the current cell in the temporary array
+                temp[j] = up + left;
+            }
+            
+            // Set the temporary array as the previous array for the next row
+            prev = temp;
+        }
+
+        // Return the number of ways to reach the bottom-right cell (m-1, n-1)
+        return prev[n - 1];
     }
 }
