d6bf8169e30951127fca2abf0c7796dae00ae95d
Om Nath Dixit <115968079+BitWiseAshu@users.noreply.github.com>
2024-08-06 12:49:24 +0530

    Time: 113 ms (43.54%), Space: 46.5 MB (11.72%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,47 +1,95 @@
+// class Solution {
+//     public String longestPalindrome(String s) {
+//         if (s == null || "".equals(s)) {
+//             return s;
+//         }
+//         int c = 0;
+//         for(int i = 0 ;i < s.length()-1 ;i++){
+//             if(s.charAt(i) != s.charAt(i+1)){
+//                 c++;
+//             }
+//         }
+
+//         if(c == 0){
+//             return s;
+//         }
+
+//         int n = s.length();
+//         boolean dp[][] =new boolean[n][n];
+//         String ans = "";
+//         for(int g = 0 ; g < n ;g++){
+//             for(int i = 0, j = g; j < n ; j++, i++){
+//                 if(g == 0){
+//                     dp[i][j] = true;
+//                 }
+//                 else if(g == 1){
+//                     if(s.charAt(i) == s.charAt(j)){
+//                         dp[i][j] = true;
+//                     }else{
+//                         dp[i][j] = false;
+//                     }
+//                 }
+//                 else{
+//                     if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true){
+//                         dp[i][j] = true;
+//                     }else{
+//                         dp[i][j] = false;
+//                     }
+//                 }
+
+//                 if(dp[i][j]){
+//                     ans = s.substring(i,j+1);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || "".equals(s)) {
-            return s;
-        }
-        int c = 0;
-        for(int i = 0 ;i < s.length()-1 ;i++){
-            if(s.charAt(i) != s.charAt(i+1)){
-                c++;
-            }
-        }
-
-        if(c == 0){
-            return s;
-        }
-
         int n = s.length();
-        boolean dp[][] =new boolean[n][n];
-        String ans = "";
-        for(int g = 0 ; g < n ;g++){
-            for(int i = 0, j = g; j < n ; j++, i++){
-                if(g == 0){
-                    dp[i][j] = true;
-                }
-                else if(g == 1){
-                    if(s.charAt(i) == s.charAt(j)){
-                        dp[i][j] = true;
-                    }else{
-                        dp[i][j] = false;
-                    }
-                }
-                else{
-                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true){
-                        dp[i][j] = true;
-                    }else{
-                        dp[i][j] = false;
-                    }
-                }
+        if(n == 0){
+            return "";
+        }
+        
+        boolean [][]dp = new boolean[n][n];
+        int start = 0;
+        int maxLength = 1;
+        
+        // All substrings of length 1 are palindromes
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
 
-                if(dp[i][j]){
-                    ans = s.substring(i,j+1);
+        // Check for substrings of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+        
+        // Check for lengths greater than 2
+        for (int L = 3; L <= n; L++) {
+            for (int i = 0; i < n - L + 1; i++) {
+                int j = i + L - 1;
+
+                // Check if the substring from i to j is a palindrome
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+
+                    if (L > maxLength) {
+                        start = i;
+                        maxLength = L;
+                    }
                 }
             }
         }
-        return ans;
+
+        return s.substring(start, start + maxLength);
     }
 }
