26e2287a500de425ea98dcc8f7bafc5cfbee2d85
Om Nath Dixit <115968079+BitWiseAshu@users.noreply.github.com>
2024-08-19 21:16:41 +0530

    Time: 19 ms (88.82%), Space: 50.6 MB (68.90%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -26,9 +26,14 @@ class Solution {
 
 
 
+
+
+// solution using memoization
+
+/*
 class Solution {
     
-    public int longestCommonSubsequenceUsingRecursion(String text1, String text2, int i, int j, int n, int m, int [][]memo){
+    public int longestCommonSubsequenceUsingMemoization(String text1, String text2, int i, int j, int n, int m, int [][]memo){
         if(i == n || j == m){
             return 0;
         }
@@ -38,11 +43,11 @@ class Solution {
         }
         
         if(text1.charAt(i) == text2.charAt(j)){
-            memo[i][j] = 1 + longestCommonSubsequenceUsingRecursion(text1, text2, i+1, j+1, n, m, memo);
+            memo[i][j] = 1 + longestCommonSubsequenceUsingMemoization(text1, text2, i+1, j+1, n, m, memo);
         }
         else{
-            int skipText1 = longestCommonSubsequenceUsingRecursion(text1, text2, i+1, j, n, m, memo);
-            int skipText2 = longestCommonSubsequenceUsingRecursion(text1, text2, i, j+1, n, m, memo);
+            int skipText1 = longestCommonSubsequenceUsingMemoization(text1, text2, i+1, j, n, m, memo);
+            int skipText2 = longestCommonSubsequenceUsingMemoization(text1, text2, i, j+1, n, m, memo);
             
             memo[i][j] = Math.max(skipText1, skipText2);
         }
@@ -57,6 +62,41 @@ class Solution {
             Arrays.fill(row, -1);
         }
         
-        return longestCommonSubsequenceUsingRecursion(text1, text2, 0, 0, text1.length(), text2.length(), memo);
+        return longestCommonSubsequenceUsingMemoization(text1, text2, 0, 0, text1.length(), text2.length(), memo);
     }
+}
+*/
+
+
+
+
+
+
+// using tabulation
+
+class Solution {
+    
+    public int longestCommonSubsequenceUsingTabulation(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Initialize the base cases (dp array is already initialized with 0s)
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        return longestCommonSubsequenceUsingTabulation(text1, text2);
+    }
+
 }
