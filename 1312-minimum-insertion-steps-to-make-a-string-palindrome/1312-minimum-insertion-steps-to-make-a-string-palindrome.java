f850198fd58dbed8106b7788cf65a233c8f03ec6
Om Nath Dixit <115968079+BitWiseAshu@users.noreply.github.com>
2024-08-24 12:48:13 +0530

    Time: 35 ms (14.74%), Space: 45.2 MB (69.25%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,3 +1,6 @@
+// Using Tabulation method
+
+/*
 class Solution {
     public int minInsertions(String s) {
         String str = new StringBuilder(s).reverse(). toString();
@@ -25,5 +28,77 @@ class Solution {
         }
         
         return (n - dp[n][n]);
+    }
+}
+*/
+
+
+
+
+
+
+
+
+// Using Recursion 
+/*
+class Solution {
+    public int minInsertions(String s) {
+        String str = new StringBuilder(s).reverse().toString();
+        
+        return (s.length() - minInsertionsHelper(s, str, s.length(), 0, 0));
+    }
+    
+    public int minInsertionsHelper(String s, String str, int n, int i, int j){
+        if(i == n || j == n){
+            return 0;
+        }
+        
+        if(s.charAt(i) == str.charAt(j)){
+            return 1 + minInsertionsHelper(s, str, n, i+1, j+1);
+        }
+        else{
+            return Math.max(minInsertionsHelper(s, str, n, i+1, j), minInsertionsHelper(s, str, n, i, j+1));
+        }
+    }
+}
+
+*/
+
+
+// Using memoization
+
+
+
+class Solution {
+    public int minInsertions(String s) {
+        String str = new StringBuilder(s).reverse().toString();
+        
+        int n = s.length();
+        
+        int dp[][] = new int[n+1][n+1];
+        for(int []arr : dp){
+            Arrays.fill(arr, -1);
+        }
+        
+        return (n - minInsertionsHelper(s, str, s.length(), 0, 0, dp));
+    }
+    
+    public int minInsertionsHelper(String s, String str, int n, int i, int j, int dp[][]){
+        if(i == n || j == n){
+            return 0;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        if(s.charAt(i) == str.charAt(j)){
+            dp[i][j] = 1 + minInsertionsHelper(s, str, n, i+1, j+1, dp);
+        }
+        else{
+            dp[i][j] = Math.max(minInsertionsHelper(s, str, n, i+1, j, dp), minInsertionsHelper(s, str, n, i, j+1, dp));
+        }
+        
+        return dp[i][j];
     }
 }
