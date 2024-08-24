fbd16f6926a4ef732a545eeba6c99279d8e1ae04
Om Nath Dixit <115968079+BitWiseAshu@users.noreply.github.com>
2024-08-24 13:10:18 +0530

    Time: 7 ms (79.47%), Space: 45.2 MB (6.21%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -31,7 +31,7 @@ class Solution {
 
 // Memoization
 
-
+/*
 class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
@@ -64,5 +64,29 @@ class Solution {
         }
         
         return dp[i][j];
+    }
+}
+*/
+
+
+
+// Tabulation 
+
+
+
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(word1.charAt(i-1) == word2.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return (n - dp[n][m]) + (m - dp[n][m]); 
     }
 }
