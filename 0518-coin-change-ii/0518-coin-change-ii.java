31dc80ae8fbb1eb2922733b572e8cd2110b82438
Om Nath Dixit <115968079+BitWiseAshu@users.noreply.github.com>
2024-08-25 12:28:23 +0530

    Time: 7 ms (46.50%), Space: 48.3 MB (12.61%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -39,7 +39,7 @@ class Solution {
 
 // memoization
 
-
+/*
 class Solution {
     
     public int changeHelper(int amount, int []coins, int index, int sum, int [][]dp){
@@ -80,5 +80,39 @@ class Solution {
         }
         
         return changeHelper(amount, coins, 0, 0, dp);
+    }
+}
+
+*/
+
+
+
+
+// tabulation
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+        
+        // Base case: There is 1 way to make the amount 0 (by not using any coins)
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        
+        // Fill the dp table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                // Exclude the current coin
+                dp[i][j] = dp[i - 1][j];
+                
+                // Include the current coin if j >= coins[i-1]
+                if (j >= coins[i - 1]) {
+                    dp[i][j] += dp[i][j - coins[i - 1]];
+                }
+            }
+        }
+        
+        return dp[n][amount];
     }
 }
