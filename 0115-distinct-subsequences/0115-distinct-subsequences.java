28139dbe6cdb007ab9055743c562fbdd06c231d0
Om Nath Dixit <115968079+BitWiseAshu@users.noreply.github.com>
2024-08-25 14:14:52 +0530

    Time: 13 ms (76.83%), Space: 48.8 MB (47.18%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -28,7 +28,7 @@ class Solution {
 
 
 // memoization 
-
+/*
 class Solution {
     
     public int numDistinctHelper(String s, String t, int i, int j, int n, int m, int [][]dp){
@@ -64,5 +64,46 @@ class Solution {
         }
         
         return numDistinctHelper(s, t, 0, 0, s.length(), t.length(), dp);
+    }
+}
+*/
+
+
+
+
+
+// Tabulation 
+
+class Solution {
+
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        
+        // dp[i][j] will store the count of distinct subsequences of t[0...j-1] in s[0...i-1]
+        int dp[][] = new int[n + 1][m + 1];
+
+        // Base case: An empty string t can be found once in any prefix of s, so initialize dp[i][0] = 1
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        // Fill the dp table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    // If characters match, include both possibilities: 
+                    // 1. Include the current character from s (dp[i-1][j-1])
+                    // 2. Exclude the current character from s (dp[i-1][j])
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    // If characters don't match, only exclude the current character from s
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        // The answer is stored in dp[n][m]
+        return dp[n][m];
     }
 }
