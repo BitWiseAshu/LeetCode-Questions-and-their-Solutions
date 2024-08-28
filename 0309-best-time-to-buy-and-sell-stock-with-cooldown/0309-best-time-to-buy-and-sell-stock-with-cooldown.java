fa348fb8cf22b7a6148c9c6b950062cf74838d2d
Om Nath Dixit <115968079+BitWiseAshu@users.noreply.github.com>
2024-08-28 15:08:46 +0530

    Time: 1 ms (84.08%), Space: 41.8 MB (33.81%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,41 @@
+class Solution {
+    
+    public int maxProfitHelper(int []prices, int index, int buy, int n, int [][]dp){
+        if(index >= n){
+            return 0;
+        }
+        
+        if(dp[index][buy] != -1){
+            return dp[index][buy];
+        }
+        
+        int take = 0;
+        int notTake = 0;
+        
+        // we can buy if buy == 1
+        if(buy == 1){
+            take = -prices[index] + maxProfitHelper(prices, index+1, 0, n, dp); // if you buy
+            notTake = 0 + maxProfitHelper(prices, index+1, 1, n, dp); // if u not buy
+        }
+        
+        // We can sell if buy == 0
+        if(buy == 0){
+            take = prices[index] + maxProfitHelper(prices, index+2, 1, n, dp); //  Sell and move to next day with buy = 1
+            notTake = 0 + maxProfitHelper(prices, index+1, 0, n, dp); // Skip selling and move to next day with buy = 0
+        }
+        
+        dp[index][buy] = Math.max(take, notTake);
+        return dp[index][buy];
+    }
+    
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int dp[][] = new int[n][2];
+        
+        for(int []arr : dp){
+            Arrays.fill(arr, -1);
+        }
+        
+        return maxProfitHelper(prices, 0, 1, n, dp);
+    }
+}
