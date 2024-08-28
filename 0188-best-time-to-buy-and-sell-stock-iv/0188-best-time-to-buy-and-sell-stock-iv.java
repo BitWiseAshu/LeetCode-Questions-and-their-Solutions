25c366cc8893c168f4d234153fcf7cbb607248a8
Om Nath Dixit <115968079+BitWiseAshu@users.noreply.github.com>
2024-08-28 14:40:28 +0530

    Time: 2 ms (82.98%), Space: 44.5 MB (49.40%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -33,9 +33,9 @@ class Solution {
 
 
 
+// Memoization 
 
-
-
+/*
 class Solution {
 
     public int maxProfitHelper(int k, int[] prices, int n, int index, int canBuy, int[][][] dp) {
@@ -51,14 +51,10 @@ class Solution {
         }
         
         if (canBuy == 1) {
-            // Try to buy at this index
             take = -prices[index] + maxProfitHelper(k, prices, n, index + 1, 0, dp);
-            // Skip buying at this index
             notTake = maxProfitHelper(k, prices, n, index + 1, 1, dp);
         } else {
-            // Try to sell at this index
             take = prices[index] + maxProfitHelper(k - 1, prices, n, index + 1, 1, dp);
-            // Skip selling at this index
             notTake = maxProfitHelper(k, prices, n, index + 1, 0, dp);
         }
         
@@ -71,12 +67,57 @@ class Solution {
         
         for (int i = 0; i < prices.length; i++) {
             for (int j = 0; j < 2; j++) {
-                for (int m = 0; m < k + 1; m++) {  // Initialize all values to -1
+                for (int m = 0; m < k + 1; m++) {
                     dp[i][j][m] = -1;
                 }
             }
         }
         
         return maxProfitHelper(k, prices, prices.length, 0, 1, dp);
+    }
+}
+*/
+
+
+
+
+
+
+
+
+// Tabulation
+class Solution {
+
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        if (n == 0) return 0;
+        int dp[][][] = new int[n + 1][2][k + 1]; 
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < 2; j++) {
+                dp[i][j][0] = 0; // No transactions left, profit is 0
+            }
+        }
+     
+        for (int index = n - 1; index >= 0; index--) {
+            for (int canBuy = 0; canBuy < 2; canBuy++) {
+                for (int capacity = 1; capacity <= k; capacity++) { // Capacity starts from 1
+                    if (canBuy == 1) { // We can buy
+                        dp[index][canBuy][capacity] = Math.max(
+                            -prices[index] + dp[index + 1][0][capacity], // Buy and move to the next day
+                            dp[index + 1][1][capacity]                  // Skip buying
+                        );
+                    } else { // We can sell
+                        dp[index][canBuy][capacity] = Math.max(
+                            prices[index] + dp[index + 1][1][capacity - 1], // Sell and move to the next day
+                            dp[index + 1][0][capacity]                     // Skip selling
+                        );
+                    }
+                }
+            }
+        }
+        
+        // The answer is dp[0][1][k], starting on day 0 with the ability to buy and k transactions left
+        return dp[0][1][k];
     }
 }
