44fbc70aea10cc353af103bcb657d2771a6879d5
Om Nath Dixit <115968079+BitWiseAshu@users.noreply.github.com>
2024-08-31 21:39:55 +0530

    Time: 367 ms (14.70%), Space: 79.5 MB (7.90%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,3 +1,78 @@
+// Recursion 
+
+/*
+class Solution {
+    
+    public int helper(int []nums, int index, int prevIndex, int n){
+        if(index == n){
+            return 0;
+        }
+        
+        int take = 0;
+        if(prevIndex == -1 || nums[index] > nums[prevIndex]){
+            take = 1 + helper(nums, index + 1, index, n);
+        }
+        
+        int notTake = helper(nums, index+1, prevIndex, n);
+        
+        return Math.max(take, notTake);
+        
+    }
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        return helper(nums, 0, -1, n);
+    }
+}
+*/
+
+
+
+
+
+// Memoization Solution 
+
+class Solution {
+    
+    public int helper(int []nums, int index, int prevIndex, int n, int dp[][]){
+        if(index == n){
+            return 0;
+        }
+        
+        if(dp[index][prevIndex+1] != -1){
+            return dp[index][prevIndex+1];
+        }
+        
+        int take = 0;
+        if(prevIndex == -1 || nums[index] > nums[prevIndex]){
+            take = 1 + helper(nums, index + 1, index, n, dp);
+        }
+        
+        int notTake = helper(nums, index+1, prevIndex, n, dp);
+        
+        dp[index][prevIndex+1] = Math.max(take, notTake);
+        return dp[index][prevIndex+1];
+        
+    }
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        
+        int dp[][] = new int[n][n+1];
+        for(int arr[] : dp){
+            Arrays.fill(arr, -1);
+        }
+        
+        return helper(nums, 0, -1, n, dp);
+    }
+}
+
+
+
+
+
+
+
+
+/*
 class Solution {
     public int lengthOfLIS(int[] nums) {
         if (nums == null || nums.length == 0) {
@@ -20,3 +95,4 @@ class Solution {
         return maxLength;
     }
 }
+*/
